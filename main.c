ccd79b02911225eed41d35c3a24abe2f0d2c6748
3SSF <muhaddithumar@gmail.com>
2024-11-09 13:56:04 +0600

    added quitting without saving the file (lol)

--- main.c
+++ main.c
@@ -9,22 +9,23 @@
 #define INS_STR "\33[93m--INSERT--"
 #define NOR_STR "\33[91m--NORMAL--"
 
-struct termios oldt, newt;
-struct winsize ws;
-int result;
+struct termios oldTerm, newTerm;
+struct winsize winSize;
+int result = 0;
 char mode[63] = NOR_STR;
-int ln; // TODO
+int lineNum = 0;
+int fileDescriptor;
 
-void set_background_color(int r, int g, int b) {
+void setBackgroundColor(int r, int g, int b) {
     printf("\033[48;2;%d;%d;%dm", r, g, b);  // Set true-color background
 }
 
-void fill_screen_with_background() {
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);  // Get terminal dimensions
+void fillScreenWithBackground() {
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &winSize);
 
     printf("\033[H");  // Move cursor to top-left
-    for (int i = 0; i < ws.ws_row; i++) {
-        for (int j = 0; j < ws.ws_col; j++) {
+    for (int i = 0; i < winSize.ws_row; i++) {
+        for (int j = 0; j < winSize.ws_col; j++) {
             putchar(' ');  // Fill each cell with a space to apply background color
         }
         putchar('\n');
@@ -32,192 +33,194 @@ void fill_screen_with_background() {
     printf("\033[H");  // Move cursor back to top-left
 }
 
-void clear() {
-    fill_screen_with_background();
+void clearScreen() {
+    fillScreenWithBackground();
 }
 
-void init() {
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+void initTerminal() {
+    tcgetattr(STDIN_FILENO, &oldTerm);
+    newTerm = oldTerm;
+    newTerm.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newTerm);
 }
 
-void normalTerm() {
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+void restoreTerminal() {
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldTerm);
     printf("\033[0m");  // Reset color to default
 }
 
-void outputString(char* out) {
-    set_background_color(30, 30, 30);  // Set a dark gray background color
-    clear();
-    printf("\33[%d;0H\33[107m", ws.ws_row-1);
-    for (int i = 0; i < ws.ws_col; i++) {
-        putchar(' ');  // Fill each cell with a space to apply background color
+void writeToFile(const char *buffer) {
+    if (fileDescriptor == -1 || buffer == NULL) return;
+
+    if (lseek(fileDescriptor, 0, SEEK_SET) == -1) {
+        perror("Error seeking to start of file");
+        return;
     }
-    printf("\33[%d;0H\33[107m", ws.ws_row-1);
-    printf(" %s | %d\33[0;0H\33[0m", mode, ln);
+
+    if (ftruncate(fileDescriptor, 0) == -1) {
+        perror("Error truncating file");
+        return;
+    }
+
+    ssize_t bytesWritten = write(fileDescriptor, buffer, strlen(buffer));
+    if (bytesWritten == -1) {
+        perror("Error writing to file");
+    }
+}
+
+void outputString(const char *out) {
+    setBackgroundColor(30, 30, 30);
+    clearScreen();
+    printf("\33[%d;0H\33[107m", winSize.ws_row-1);
+
+    for (int i = 0; i < winSize.ws_col; i++) {
+        putchar(' ');
+    }
+    printf("\33[%d;0H\33[107m", winSize.ws_row-1);
+    printf(" %s | %d\33[0;0H\33[0m", mode, lineNum);
     printf("\n\033[48;2;30;30;30m\033[38;2;50;50;50m ~ \033[0m\033[48;2;30;30;30m");
-    for (int i = 0; i < strlen(out); i++) {
+
+    for (size_t i = 0; i < strlen(out); i++) {
         if (out[i] == '\n') {
-            // Set both the foreground and background colors for the "~ " part
             printf("\n\033[48;2;30;30;30m\033[38;2;50;50;50m ~ \033[0m\033[48;2;30;30;30m");
         } else {
             putchar(out[i]);
         }
     }
-    
-    // Reset to avoid affecting any output after this function
     printf("\033[0m");
 }
 
-char* program(char* fileContents) {
-    char broken = 0;
+void programLoop(char *fileContents) {
     size_t initialSize = strlen(fileContents);
-    char* inp = (char*)malloc(initialSize + 2);
-    if (inp == NULL) {
+    char *inputBuffer = (char *)malloc(initialSize + 2);
+    if (inputBuffer == NULL) {
         result = 255;
-        return NULL;
+        return;
     }
 
-    strcpy(inp, fileContents);
-    int i = initialSize; 
+    strcpy(inputBuffer, fileContents);
+    int index = initialSize;
+    int breakLoop = 0;
 
-    char buf = 0;
-    outputString(inp);
+    outputString(inputBuffer);
+
     while (1) {
-        buf = getchar();
-        if (!strcmp(mode, INS_STR)){
-            if (i > 0 && buf == 127) {
-                inp[--i] = '\0';
+        char buf = getchar();
+
+        if (!strcmp(mode, INS_STR)) {
+            if (index > 0 && buf == 127) {
+                inputBuffer[--index] = '\0';
             } else if (buf == 27) { 
                 strcpy(mode, NOR_STR);
-            } else if (buf != 127) {
-                inp[i++] = buf;
+            } else if (buf != 127 && index < (initialSize + 10000)) {  // Prevent buffer overflow
+                inputBuffer[index++] = buf;
+                inputBuffer[index] = '\0';  // Null-terminate the string
             }
         } else {
-            if (buf == 'i') strcpy(mode, INS_STR);
-            if (buf == ':'){
-                printf("\33[%d;0H", ws.ws_row);
-                printf(":");
+            if (buf == 'i') {
+                strcpy(mode, INS_STR);
+            }
+            if (buf == ':') {
+                printf("\33[%d;0H:", winSize.ws_row);
+                fflush(stdout);  // Ensure prompt is displayed
                 char buf2;
                 int i2 = 0;
-                char inp2[255] = {'\0'};
-                while (1){
+                char inputCommand[255] = {'\0'};
+
+                while (1) {
                     buf2 = getchar();
                     if (buf2 == 27) break;
-                    else if (buf2 == 127 && i2 > 0){
-                        inp2[--i2] = '\0';
-                    } else if (buf2 == '\n'){
-                        if (!strcmp(inp2, "wq")){
-                            broken = 1;
+                    else if (buf2 == 127 && i2 > 0) {
+                        inputCommand[--i2] = '\0';
+                    }
+                    else if (buf2 == '\n' || buf2 == '\r') {
+                        inputCommand[i2] = '\0';
+                        if (!strcmp(inputCommand, "wq") || !strcmp(inputCommand, "x")) {
+                            writeToFile(inputBuffer);
+                            breakLoop = 1;
                             break;
                         }
-                        if (!strcmp(inp2, "x")){
-                            broken = 1;
+                        if (!strcmp(inputCommand, "q")) {
+                            breakLoop = 1;
                             break;
                         }
-                        if (inp2[0] == '!'){
-                            inp2[0] = '\0';
-                            for (i = 0; i < 256; i++){
-                                inp2[i] = inp2[i] ^ inp2[i+1];
-                                inp2[i+1] = inp2[i+1] ^ inp2[i];
-                                inp2[i] = inp2[i] ^ inp2[i+1];
-                            }
-                            system(inp2);
-                            break;
+                        break;
+                    }
+                    else {
+                        if (i2 < sizeof(inputCommand) - 1) {  // Prevent buffer overflow
+                            inputCommand[i2++] = buf2;
+                            inputCommand[i2] = '\0';
                         }
-                    } else inp2[i2++] = buf2;
-                    outputString(inp);
-                    printf("\33[%d;0H\033[0m\033[48;2;30;30;30m:%s", ws.ws_row, inp2);
+                    }
+
+                    outputString(inputBuffer);
+                    printf("\33[%d;0H\033[0m\033[48;2;30;30;30m:%s", winSize.ws_row, inputCommand);
+                    fflush(stdout);  // Ensure command input is displayed
                 }
             }
         }
 
-        inp[i] = '\0';
-        char* tmp = realloc(inp, i + 2);
+        // Reallocate buffer to accommodate new characters
+        inputBuffer[index] = '\0';  // Ensure null-termination
+        char *tmp = realloc(inputBuffer, index + 2);
         if (tmp == NULL) {
             result = 16;
-            free(inp);
-            return NULL;
+            free(inputBuffer);
+            return;
         }
-        inp = tmp;
-        outputString(inp);
-        if (broken) break;
+        inputBuffer = tmp;
+        outputString(inputBuffer);
+        if (breakLoop) break;
     }
-
-    return inp;
 }
 
-int main(int argc, char* argv[]) {
-    if (argv[1] == NULL) return 5;
+int main(int argc, char *argv[]) {
+    if (argc < 2) return 5;
 
-    int fd = open(argv[1], O_RDWR | O_CREAT, 0644);
-    if (fd == -1) {
+    fileDescriptor = open(argv[1], O_RDWR | O_CREAT, 0644);
+    if (fileDescriptor == -1) {
         perror("Error opening file");
         return 23;
     }
 
-    init();
-    set_background_color(30, 30, 30);  // Set a dark gray background color
-    fill_screen_with_background();
+    initTerminal();
+    setBackgroundColor(30, 30, 30);
+    fillScreenWithBackground();
 
-    off_t fileSize = lseek(fd, 0, SEEK_END);
+    off_t fileSize = lseek(fileDescriptor, 0, SEEK_END);
     if (fileSize == -1) {
         perror("Error getting file size");
-        close(fd);
+        close(fileDescriptor);
         return 125;
     }
 
-    lseek(fd, 0, SEEK_SET);
+    if (lseek(fileDescriptor, 0, SEEK_SET) == -1) {
+        perror("Error resetting file offset");
+        close(fileDescriptor);
+        return 125;
+    }
 
-    char* fC = (char*)malloc(fileSize + 1);
-    if (fC == NULL) {
+    char *fileContents = (char *)malloc(fileSize + 1);
+    if (fileContents == NULL) {
         perror("Unable to allocate memory for file contents");
-        close(fd);
+        close(fileDescriptor);
         return 132;
     }
 
-    ssize_t buf = read(fd, fC, fileSize);
-    if (buf == -1) {
+    ssize_t bytesRead = read(fileDescriptor, fileContents, fileSize);
+    if (bytesRead == -1) {
         perror("Unable to read file contents");
-        close(fd);
-        free(fC);
+        close(fileDescriptor);
+        free(fileContents);
         return 22;
     }
-    
-    fC[buf] = '\0';
 
-    lseek(fd, 0, SEEK_SET);
+    fileContents[bytesRead] = '\0';
+    programLoop(fileContents);
+    free(fileContents);
 
-    char* dat = program(fC);
-    free(fC);
-    if (dat == NULL) {
-        close(fd);
-        normalTerm();
-        return result;
-    }
-
-    if (ftruncate(fd, 0) == -1) {
-        perror("Error truncating file");
-        close(fd);
-        free(dat);
-        normalTerm();
-        return 1;
-    }
-
-    ssize_t bytesWritten = write(fd, dat, strlen(dat));
-    if (bytesWritten == -1) {
-        perror("Error writing to file");
-        close(fd);
-        free(dat);
-        normalTerm();
-        return 1;
-    }
-
-    close(fd);
-    normalTerm();
-    free(dat);
+    close(fileDescriptor);
+    restoreTerminal();
 
     return result;
 }
